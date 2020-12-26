7398d3da0393191d5e2813fd4643613af073c8e9
Tom Drew <tomdrew@Toms-MacBook-Air.local>
2020-12-26 22:45:06 +0000

    produces correct answwer

---
+++ Day_1/sum_puzzle_input.py
@@ -0,0 +1,26 @@
+from itertools import permutations
+
+with open("input.txt", "r") as f:
+    expenses = [ int(i) for i in f ]
+
+numbers = expenses
+target_number = 2020
+
+solutions = [pair for pair in permutations(numbers, 2) if sum(pair) == target_number]
+print('Solutions:', solutions)
+
+print(solutions[1])
+
+int1, int2 = (solutions[1])
+result = (int1 * int2 )
+
+print(result)
+
+
+
+
+
+
+
+
+
