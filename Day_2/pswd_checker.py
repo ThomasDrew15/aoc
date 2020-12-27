44acc467308b0c551b45a1fbd5fb58fe91fe524c
Tom Drew <ihavenolife@hotmail.co.uk>
2020-12-27 12:15:28 +0000

    readability changes

--- Day_2/pswd_checker.py
+++ Day_2/pswd_checker.py
@@ -1,6 +1,6 @@
 #open input text file from AOC holding possible answers
 #read each line into a list
-with open("inputd2.txt", "r") as f:
+with open("input2.txt", "r") as f:
     pswds = [ int(i) for i in f ]
 
 print(pswds[0])
