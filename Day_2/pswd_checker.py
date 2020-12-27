a59a2e9c69bf70468c40d4399b63004ebe3e174a
Tom Drew <ihavenolife@hotmail.co.uk>
2020-12-27 17:24:12 +0000

    finished and working pt1

--- Day_2/pswd_checker.py
+++ Day_2/pswd_checker.py
@@ -3,6 +3,7 @@
 with open("test_strings.txt", "r") as f:
     pswds = [ str(i) for i in f ]
 
+
 validPasswords = 0
 invalidPasswords = 0
 notInString = 0
@@ -13,21 +14,25 @@ for i in pswds:
     print(string)
 
 
-for i in test_strings:
-    string = str(i)
-    print(string)
-
         #split strong by whitepsace
     stringsplit = string.split()
-#print(stringsplit)
+    #print(stringsplit, "split string")
 
         #create substring of letter
     substring = stringsplit[1]
 #print(substring[0])
 
+        #create substring, letter only
+    keyLetter = substring[0]
+    print(keyLetter)
+
+        #create string from stringSplit[2]
+    keyString = stringsplit[2]
+    print(keyString)
+
         #count how often substring letter appears in password
     count =stringsplit[2].count(substring[0])
-#print(count)
+    #print(count, "count of how often letter appears")
 
         #get parameters for how often letter should appear
     countValues = stringsplit[0]
@@ -40,22 +45,21 @@ for i in test_strings:
         #assign values to variables
     lowValue = int(splitCountValues[0])
     highValue = int(splitCountValues[1])
-#print(lowValue, "and", highValue)
+    #print("values are ", lowValue, "and", highValue)
 
-
+    #if/else statement to decide if password valid
     if substring[0] not in stringsplit[2]:
         print("not in string")
         notInString +=1
-        continue
         #if/else statement to decide if password valid
         #password is valid if letter is counted between low and high values
-    elif count >= highValue or count <= lowValue:
+    elif count > highValue or count < lowValue:
         print("password not valid")
         invalidPasswords +=1
     else:
         print("password valid")
         validPasswords +=1
-        
 
+#print(len(pswds))
 totalPasswords = validPasswords + invalidPasswords + notInString
 print("there are:", validPasswords, "valid passwords", invalidPasswords, "invalid passwords and", notInString, "not in string", totalPasswords, "all together")
