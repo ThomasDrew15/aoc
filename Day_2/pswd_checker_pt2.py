61f18f89eec045cdd8687f46a75939119ce2d475
Tom Drew <ihavenolife@hotmail.co.uk>
2020-12-27 21:07:19 +0000

    completed!

--- Day_2/pswd_checker_pt2.py
+++ Day_2/pswd_checker_pt2.py
@@ -1,38 +1,46 @@
 #open input text file from AOC holding possible answers
 #read each line into a list
-with open("test_strings.txt", "r") as f:
+#with open("test_strings.txt", "r") as f:
+with open("passes.txt", "r") as f:
     pswds = [ str(i) for i in f ]
 
 
 validPasswords = 0
-invalidPasswords = 0
+repeats = 0
 notInString = 0
+errors = 0
 
 
 for i in pswds:
     string = str(i)
-    print(string)
+    #print(string)
 
 
         #split strong by whitepsace
     stringsplit = string.split()
-    #print(stringsplit, "split string")
+#print(stringsplit, "split string")
 
         #create substring of letter
     substring = stringsplit[1]
 #print(substring[0])
 
+
+
         #create substring, letter only
     keyLetter = substring[0]
-    print(keyLetter)
+#print(keyLetter)
 
         #create string from stringSplit[2]
     keyString = stringsplit[2]
-    print(keyString)
+#print(keyString)
+
+
 
         #count how often substring letter appears in password
     count =stringsplit[2].count(substring[0])
-    #print(count, "count of how often letter appears")
+#print(count, "count of how often letter appears")
+
+
 
         #get parameters for how often letter should appear
     countValues = stringsplit[0]
@@ -45,34 +53,39 @@ for i in pswds:
         #assign values to variables
     lowValue = int(splitCountValues[0])
     highValue = int(splitCountValues[1])
-    #print("values are ", lowValue, "and", highValue)
+    print("values are ", lowValue, "and", highValue)
 
-    #if/else statement to decide if password valid
-    if substring[0] not in stringsplit[2]:
+        #show what indexes are relevant 
+    print(keyString, "is the key string")
+    print(keyLetter, "is the key letter")
+
+        #get charater at first index value
+    indexValueLow = keyString[lowValue -1]
+    print(indexValueLow, "low value index")
+
+        #get character at second index value
+    indexValueHigh = keyString[highValue -1]
+    print(indexValueHigh, "high value index")
+
+        #if the key letter is not present in the string, finish
+    if keyLetter not in keyString:
         print("not in string")
         notInString +=1
-        #if/else statement to decide if password valid
-        #password is valid if letter is counted between low and high values
-    elif count > highValue or count < lowValue:
-        print("password not valid")
-        invalidPasswords +=1
+        continue
     else:
-        print("password valid")
-        validPasswords +=1
-
-#print(len(pswds))
-totalPasswords = validPasswords + invalidPasswords + notInString
-print("there are:", validPasswords, "valid passwords", invalidPasswords, "invalid passwords and", notInString, "not in string", totalPasswords, "all together")
-
-        #pt2: psotions of letter must match low/high values
-        #get index positions of substring, -1 is a failure
-        indexPositions = keyString.find(keyLetter)
-        print("these are the index positions ", indexPositions)
-        if indexPositions == -1:
-            #continue to move on to next
+            #if the character at both index values is the same, finish
+        if indexValueLow == indexValueHigh:
+            print("cannot repeat at both indexes")
+            repeats +=1
             continue
         else:
-            if indexPositions == splitCountValues:
+            if indexValueHigh == keyLetter or indexValueLow == keyLetter:
+                #passes
                 print("pass")
-            else:
-                print("failed")
+                validPasswords +=1
+
+print(validPasswords, "valid passwords...", repeats, "repeats...", notInString, "not in string...")
+
+
+    
+            
